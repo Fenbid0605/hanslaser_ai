f36bb7425914388667397860c7a64b52f40e74d8
shousijichi <befang1122@163.com>
2022-01-29 10:42:47 +0800

    更新后向代码

--- backward/config.py
+++ backward/config.py
@@ -2,21 +2,21 @@
 import torch
 import datetime
 N_INPUT = 3
-N_HIDDEN_LAYER = 12
-N_HIDDEN = 20
+N_HIDDEN_LAYER = 8
+N_HIDDEN = 34
 N_OUTPUT = 4
 ACTIVATION = torch.relu
 B_INIT = -0.2  # use a bad bias constant initializer
-EPOCH = 1e5
-LR = 1e-4
+EPOCH = 10e4
+LR = 1e-5
 
 EPOCH = int(EPOCH)
 
 
 def save_config(loss):
     with open('./result/config_log.txt', "a+") as f:  # 只需要将之前的”w"改为“a"即可，代表追加内容
-        f.write(f"N_HIDDEN: {N_HIDDEN} \n")
         f.write(f"N_HIDDEN_LAYER: {N_HIDDEN_LAYER} \n")
+        f.write(f"N_HIDDEN: {N_HIDDEN} \n")
         f.write(f"EPOCH: {EPOCH} \n")
         f.write(f"LR: {LR} \n")
         f.write(f"LOSS: {loss} \n")
--- backward/main.py
+++ backward/main.py
@@ -38,26 +38,30 @@ def train(_model, dataSet):
         train_loss = loss_func(prediction, y).to(device)
         train_loss_list.append(train_loss.item())
 
-        # prediction = net(v_x).to(device)
-        # valid_loss = loss_func(prediction, v_y).to(device)
-        # valid_loss_list.append(valid_loss.item())
+        prediction = net(v_x).to(device)
+        net.eval()
+        valid_loss = loss_func(prediction, v_y).to(device)
+        valid_loss_list.append(valid_loss.item())
+        net.train()
 
         optimizer.zero_grad()
         train_loss.backward()
         optimizer.step()
 
         x_list.append(i)
+        if i % 9999 == 0:
+            print(f"EPOCH: {i} ,train_loss: {train_loss.item()} , valid_loss: {valid_loss.item()}")
 
     # 绘图
     fig, ax = plt.subplots()
 
     ax.plot(x_list, train_loss_list, label='train_loss')
-    # ax.plot(x_list, valid_loss_list, label='valid_loss')
+    ax.plot(x_list, valid_loss_list, label='valid_loss')
     ax.legend()
 
     loss = train_loss_list[len(train_loss_list) - 4:]
     print(f'loss:{loss}')
-    config.save_config(loss=loss)
+    config.save_config(loss=loss) #记录参数日志
 
     plt.savefig('./result/loss.png')
     fig.suptitle('Loss')
@@ -67,6 +71,12 @@ def train(_model, dataSet):
 if __name__ == '__main__':
     model = Net()
     # print(model)
+
+    try:
+        model.load_state_dict(torch.load('model.pl', map_location=device))
+        model.train()
+    except:
+        pass
 
     dataSet = DataSet()
 
