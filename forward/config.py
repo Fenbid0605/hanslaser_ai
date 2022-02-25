d50c8392b494b61e17617d6b835ad7c498c2e73a
0xJacky <me@jackyu.cn>
2022-02-25 10:40:50 +0800

    update forward net params

--- forward/config.py
+++ forward/config.py
@@ -4,13 +4,13 @@ import datetime
 import torch
 
 N_INPUT = 4
-N_HIDDEN_LAYER = 30
+N_HIDDEN_LAYER = 36
 N_HIDDEN = 130
 N_OUTPUT = 3
 ACTIVATION = torch.tanh
 B_INIT = -0.2  # use a bad bias constant initializer
-EPOCH = 1e4
-LR = 6e-5
+EPOCH = 6e4
+LR = 1e-5
 
 EPOCH = int(EPOCH)
 
